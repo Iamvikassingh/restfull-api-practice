38cd446e26ac153c69e0780c259b6f7f30b6bf2e
Iamvikassingh <9625vikassingh@gmail.com>
2024-09-08 22:02:46 +0530

    new practice with mongodb

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const mongoose = require("mongoose");
+
 
 const app = express();
 const PORT = 8000;
+
+
+
+// connection with mongoDB
+
+mongoose.connect("mongodb://127.0.0.1:27017/youtube-app-1")
+    .then(() => {
+        console.log("connected to mongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+
+    },
+    last_name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    jobTitle: {
+        type: String,
+        required: true,
+    }
+},{timestamps:true})
+
+const User = mongoose.model("Users", userSchema);
 
 app.use(express.urlencoded({ extended: false })); //middleware
 
@@ -27,13 +66,6 @@ app.route('/api/users/:id')
         return resp.json(user)
     })
     .patch((req, resp) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        user.push({...body , id});
-        fs.appendFile("./MOCK_DATA.json",JSON.stringify(users), (err,data)=>{
-            if(err) console.error(err);
-            else console.log(data);
-        })
         return resp.json({ status: "pending" });
     })
     .delete((req, resp) => {
@@ -41,16 +73,24 @@ app.route('/api/users/:id')
     })
 
 
-app.post("/api/users", (req, resp) => {
+app.post("/api/users", async (req, resp) => {
     const body = req.body;
     console.log("body", body)
-    users.push({ ...body, id: users.length + 1 });
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return resp.json({ status: "success", id: users.length });
-    })
+    // users.push({ ...body, id: users.length + 1 });
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    //     return resp.json({ status: "success", id: users.length });
+    // })
+    await User.create({
+        first_name:body.first_name,
+        last_name:body.last_name,
+        jobTitle:body.jobTitle,
+        email:body.email
+    });
+    return resp.status(200).json({ status: "success", id: users.length });
 })
 
 
 
 app.listen(PORT, () => console.log(`server is starting on the PORT : ${PORT}`)
 );
+
